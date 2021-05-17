31068c144fe628a12a658c74859772d36d5d1f68
Raskolnikov <jerry.tann@opteyes.cn>
2021-05-17 16:27:50 +0800

    feat: implement logic of filter box

--- react/debounce-and-throttle/src/components/fc.js
+++ react/debounce-and-throttle/src/components/fc.js
@@ -1,36 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 function getRand() {
-    return Math.round(Math.random() * 100)
+  return Math.round(Math.random() * 1000);
 }
 
 export default function FunctionalComponent(props) {
-    const list = new Array(20).fill(0).map(getRand)
-    console.log('list is ', list)
-    const [l] = useState(list)
-    const [ld, setLd] = useState(l.filter(_ => true))
-    const [searchStr, setSearchStr] = useState('')
-    const searchStrChanged = (input) => {
-        setSearchStr(input.target.value)
-    }
-    const renderList = () => {
-      return (
-          ld.map((i, idx) => (
-              <li key={idx}>
-                {i}
-              </li>
-          ))
-      )
-    }
+  const list = new Array(20).fill(0).map(getRand);
+  console.log("list is ", list);
+  const [l] = useState(list);
+  const [ld, setLd] = useState(l.filter(_ => true));
+  const [searchStr, setSearchStr] = useState("5");
+  const searchStrChanged = input => {
+    setSearchStr(input.target.value);
+    console.log("searchStr is now ", searchStr);
+  };
+  useEffect(() => {
+    const filtered = l.filter(i => {
+      console.log("searchStr is ", searchStr);
+      console.log("i to string", i.toString());
+      return i.toString().includes(searchStr.toString());
+    });
+    setLd(filtered)
+    console.log("filtered is now ", filtered)
+  }, [searchStr, l]);
+  const renderList = () => {
+      console.log('ld is ', ld)
+    return ld.map((i, idx) => <li key={idx}>{i}</li>);
+  };
 
-    return <div>
-        <div>filter: 
-            <input value={searchStr} onChange={searchStrChanged}/>
-        </div>
-        <div><span>list: </span>
-          <ul>
-              { renderList() }
-          </ul>
-        </div>
+  return (
+    <div style={{ margin: "10px" }}>
+      <div>
+        filter:
+        <input value={searchStr} onChange={searchStrChanged} />
+      </div>
+      <div>
+        <span>list: </span>
+        <ul>{renderList()}</ul>
+      </div>
     </div>
+  );
 }
