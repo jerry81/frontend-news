e7023c46409a2d96aed2ca947b842df94cb75792
Raskolnikov <jerry.tann@opteyes.cn>
2021-06-22 12:44:01 +0800

    feat: add hoisting test for variable declared with var

--- miscJs/hoisting.js
+++ miscJs/hoisting.js
@@ -12,7 +12,13 @@ const asObj = function () {
 
 function withDecl() {
     console.log('i am in withDecl')
+    try {
     console.debug('var test ', notHoisted)
+    } catch(e) {
+        console.error('we tried to access variable declared with let')
+    }
+    console.debug('hosisted declared with var ', hoisted) // but it it is not initialized
 }
 
 let notHoisted = 'notHoisted'
+var hoisted = 'hoisted'
