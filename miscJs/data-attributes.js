ad3ce2575e42615d23df4213621ba761d78e0478
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-10 21:28:49 +0800

    feat: bubbling and stop propagation

--- miscJs/data-attributes.js
+++ miscJs/data-attributes.js
@@ -6,13 +6,28 @@ let cbutt = document.querySelector('.cbut')
 cbutt.addEventListener('click', cbHandler)
 
 function gHandler(e) {
+  e.stopPropagation()
   console.dir(e)
   e.target.innerHTML = `buttonD-${e.target.dataset['a']}`
 }
 
+let nostop = document.querySelector('.noStop')
+nostop.addEventListener('click', nostopHandler)
+
+function nostopHandler() {
+  console.log('buttons handler')
+}
+
 function cbHandler(e) {
+  e.stopPropagation()
   console.log('tar', e.target)
   console.log('e curTar', e.currentTarget)
   e.target.innerText = `target is ${e.target.outerHTML}
   and currentTarget is ${e.currentTarget.outerHTML}`
+}
+
+window.addEventListener('click', wHandler)
+
+function wHandler(e) {
+  console.log('global handler clicked', e)
 }
