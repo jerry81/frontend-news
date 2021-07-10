5123f8c12af9aa2c17c72d7c595598193cca565c
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-10 21:47:13 +0800

    feat: capture and this example

--- miscJs/data-attributes.js
+++ miscJs/data-attributes.js
@@ -22,12 +22,22 @@ function cbHandler(e) {
   e.stopPropagation()
   console.log('tar', e.target)
   console.log('e curTar', e.currentTarget)
+  console.log('finally, this is ', this) // same as currentTarget, not recommended
   e.target.innerText = `target is ${e.target.outerHTML}
   and currentTarget is ${e.currentTarget.outerHTML}`
 }
 
-window.addEventListener('click', wHandler)
+window.addEventListener('click', wHandler, 
+  {capture:true}
+)
 
 function wHandler(e) {
   console.log('global handler clicked', e)
+}
+
+let ub = document.querySelector('.unBlock')
+ub.addEventListener('click', unblockH)
+
+function unblockH() {
+  window.removeEventListener('click', wHandler)
 }
