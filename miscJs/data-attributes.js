b6b5d651ba4eb8a6c3f72430e3315498024468a2
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-10 21:17:20 +0800

    feat: currentTarget vs target example

--- miscJs/data-attributes.js
+++ miscJs/data-attributes.js
@@ -2,8 +2,17 @@ let butts = document.querySelectorAll('.but')
 butts.forEach(but => {
   but.addEventListener('click', gHandler)
 })
+let cbutt = document.querySelector('.cbut')
+cbutt.addEventListener('click', cbHandler)
 
 function gHandler(e) {
   console.dir(e)
   e.target.innerHTML = `buttonD-${e.target.dataset['a']}`
+}
+
+function cbHandler(e) {
+  console.log('tar', e.target)
+  console.log('e curTar', e.currentTarget)
+  e.target.innerText = `target is ${e.target.outerHTML}
+  and currentTarget is ${e.currentTarget.outerHTML}`
 }
