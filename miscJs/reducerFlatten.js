06cb36845e4024b1321e6d53d52ccb69e29640e5
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-19 19:15:58 +0800

    feat: add reducer example - count occurences of characters

--- miscJs/reducerFlatten.js
+++ miscJs/reducerFlatten.js
@@ -8,3 +8,21 @@ function reducer(acc, cur) {
 }
 const reduced = original.reduce(reducer, []);
 console.log("reduced", reduced);
+
+
+const str = 'we want to make sure that whether the letter is uppercase or lowercase, we still only count it once. For example a and A would could as two "a"s, not one uppercase A and one lowercase a.'
+asList = str.split('')
+
+function countCharCount(acc, cur) {
+  if (!acc[cur]) {
+    acc[cur] = 1
+  } else {
+    acc[cur]++
+  }
+  return acc
+}
+const countMap = asList.reduce(countCharCount, {})
+console.log('countMap is ', countMap)
+
+const el = document.querySelector('#countMapOutput')
+el.innerHTML = JSON.stringify(countMap)
