cd014c0264c411ac959d1305548db561e469905a
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-20 11:01:33 +0800

    feat: example with async await

--- miscJs/asyncawait.js
+++ miscJs/asyncawait.js
@@ -1,4 +1,4 @@
-function wait(t) {
+async function wait(t) {
   return new Promise(res => {
     setTimeout(res, t);
   });
@@ -7,3 +7,9 @@ function wait(t) {
 wait(5000).then(() => {
   console.log("im done");
 });
+
+// with async await - no nesting
+(async () => { 
+  await wait(2000);
+  console.log("this is with async await");
+})()
