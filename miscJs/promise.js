1c17d7f1db259b82d88d696e6b0d86dd9a3fffd4
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-20 10:10:07 +0800

    feat: add promise all example

--- miscJs/promise.js
+++ miscJs/promise.js
@@ -1,10 +1,21 @@
 const pr = new Promise((res, _) => {
-    setTimeout(() => {
-      console.log('promise success after 3 s')
-      res()
-    }, 3000)
-  })
-  pr.then(() => {
-    console.log('after resolve')
-  })
-  console.log('immediately after')
+  setTimeout(() => {
+    console.log("promise success after 3 s");
+    res();
+  }, 3000);
+});
+pr.then(() => {
+  console.log("after resolve");
+});
+console.log("immediately after");
+
+const pr2 = new Promise(res => {
+  setTimeout(() => {
+    console.log("after 1 second, pr2");
+    res();
+  });
+});
+Promise.all([pr, pr]).then(() => {
+  console.log("all promises completed");
+});
+console.log("immediately after promise all");
