1194cfd35fae06c8a4153bf89ead8401a7a5abc7
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-21 10:09:07 +0800

    feat: finish public api tutorial

--- miscJs/publicapi.js
+++ miscJs/publicapi.js
@@ -2,12 +2,12 @@ const base = 'https://cat-fact.herokuapp.com'
 const facts = '/facts'
 const wa = document.querySelector('#work-area')
 
+let reader
 async function getFact() {
   const res = await fetch(`${base}${facts}`)
-  console.log('res is ', res)
-  wa.innerHTML = `<button onclick="getNextFact()">get next fact</button>`  
-}
-
-async function getNextFact() {
-  console.log('get next fact called')
+  const factsArr = await res.json()
+  console.log('factsArr', factsArr)
+  factsArr.forEach((fact) => {
+    wa.innerHTML += `<div>${fact.text}</div>`
+  })
 }
