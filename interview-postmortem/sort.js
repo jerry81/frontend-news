58e7bb9472f629b0423b2affe10e1d6cb1346825
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-05 20:12:55 +0800

    feat: add solution for closest subArray to a sum

--- interview-postmortem/sort.js
+++ interview-postmortem/sort.js
@@ -26,9 +26,52 @@ function reduceSort(A) {
         return accum.splice(i, 0, cur);
       }
     }
-    accum.push(cur)
-    return accum
+    accum.push(cur);
+    return accum;
   }, []);
 }
 
 console.log("reduceSort", reduceSort(input));
+
+function closestSub(sum, A) {
+  /* starting with largest, add*/
+  const sorted = sort(A);
+  let sol = [];
+  let curSum = 0;
+  let curDiff = Number.MAX_VALUE;
+  let smallestOverSum = Number.MAX_VALUE;
+  let overSol = [];
+  for (let i = sorted.length - 1; i >= 0; i--) {
+    if (sorted[i] > sum) {
+      if (sorted[i] < smallestOverSum) {
+        smallestOverSum = sorted[i];
+        overSol = [sorted[i]];
+      }
+    }
+    let potential = sorted[i] + curSum
+    if (potential <= sum) {
+      sol.push(sorted[i]);
+      curSum += sorted[i];
+      curDiff = Math.abs(sum - curSum);
+    } else {
+        let potentialDiff = Math.abs(potential - sum)
+        if (potentialDiff < curDiff) {
+            overSol = [...sol, sorted[i]]
+            smallestOverSum = potential
+        }
+    }
+  }
+  if (Math.abs(smallestOverSum - sum) < curDiff) {
+    sol = overSol
+  }
+  return sol;
+}
+
+const input2 = [24, 25, 100];
+const input3 = [24, 25, 32, 100];
+console.log("closestSub ", closestSub(13, input));
+console.log("closest 2", closestSub(80, input2)); // expect 100
+console.log("closest 3", closestSub(80, input3));
+/* 
+  react event run immediately
+*/
